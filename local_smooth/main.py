043310edc9402c429956583964ca677af81d07c5
sverdoot <lagutin.em@phystech.edu>
2022-03-21 14:05:07 +0300

    add logging

--- local_smooth/main.py
+++ local_smooth/main.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 from typing import Any, Dict
+import logging
 
 import numpy as np
 import ruamel.yaml as yaml
@@ -12,6 +13,10 @@ from local_smooth.risk import true_risk, unbiased_risk_estimate
 from local_smooth.utils import random_seed
 from local_smooth.vizualize import plot_data, plot_true_risk
 
+#logger = logging.getLogger()
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -21,8 +26,7 @@ def parse_arguments():
 
 
 def main(config: Dict[str, Any], args: argparse.Namespace):
-    print('Config: ')
-    print(yaml.safe_dump(config, default_flow_style=False))
+    logging.info(f'Config: \n{yaml.safe_dump(config, default_flow_style=False)}')
     
     if config["seed"]:
         random_seed(config["seed"])
@@ -35,7 +39,7 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
     ys = data_model.sample_ys(xs, config["seed"])
 
     x0 = np.array(np.random.rand())
-    print(f"x_0: {x0:.3f}")
+    logging.info(f"x_0: {x0:.3f}")
 
     Path(config["figpath"]).mkdir(exist_ok=True)
     plot_data(
@@ -49,10 +53,8 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
     grad_x0 = data_model.grad_func(x0)
     f_x0 = data_model.func(x0)
     bandwidths = np.logspace(
-        config["bandwidth_max"], config["bandwidth_min"], config["num_bandwidths"]
+        config["bandwidth_max_log"], config["bandwidth_min_log"], config["num_bandwidths"]
     )
-
-
 
     local_smother = LocalSmoothing(config["order"])
     kernel = KernelFactory.get_kernel(config["kernel"])
@@ -60,19 +62,19 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
     risk_estimates = np.empty(bandwidths.shape[0])
     true_risks = np.empty(bandwidths.shape[0])
     for bandwidth_idx, bandwidth in enumerate(bandwidths):
-        print(f"\nBandwidth: {bandwidth}")
+        logging.info(f"\nBandwidth: {bandwidth}")
         weights = kernel(xs - x0, bandwidth)
         local_smother.fit(x0, xs, ys, weights)
 
-        print(f"Function estimate: {local_smother.estimate[0]:.3f}, true value: {f_x0:.3f}")
-        print(f"Derivative estimate: {local_smother.estimate[1]:.3f}, true value: {grad_x0:.3f}")
+        logging.info(f"Function estimate: {local_smother.estimate[0]:.3f}, true value: {f_x0:.3f}")
+        logging.info(f"Derivative estimate: {local_smother.estimate[1]:.3f}, true value: {grad_x0:.3f}")
 
         weights = kernel(xs[:, None] - xs[None, :], bandwidth)
         kernel_matrix = local_smother.construct_kernel_matrix(xs, weights)
         risk_estimate = unbiased_risk_estimate(kernel_matrix, ys, config["sigma"])
         risk_estimates[bandwidth_idx] = risk_estimate
 
-        print(f"Unbiased risk estimate: {risk_estimate:.3f}")
+        logging.info(f"Unbiased risk estimate: {risk_estimate:.3f}")
 
         weights = kernel(xs - x0, bandwidth)
         true_risk_ = true_risk(
@@ -80,8 +82,8 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
         )
         true_risks[bandwidth_idx] = true_risk_
 
-        print(f"True risk: {true_risk_:.3f}")
-    print("\n")
+        logging.info(f"True risk: {true_risk_:.3f}")
+    logging.info("\n")
 
     best_idx = np.argmin(risk_estimates)
     best_bandwidth_emp = bandwidths[best_idx]
@@ -91,8 +93,8 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
     optimal_bandwidth = bandwidths[optimal_idx]
     optimal_risk = true_risks[optimal_idx]
 
-    print(f"Oracle bandwidth: {optimal_bandwidth:.5f}, oracle risk: {optimal_risk:.3f}")
-    print(
+    logging.info(f"Oracle bandwidth: {optimal_bandwidth:.5f}, oracle risk: {optimal_risk:.3f}")
+    logging.info(
         f"Chosen bandwidth: {best_bandwidth_emp:.5f}, risk: {true_risks[best_idx]:.3f}"
     )
 
@@ -110,7 +112,7 @@ def main(config: Dict[str, Any], args: argparse.Namespace):
         savepath=Path(config["figpath"], f"true_risk_{config['name']}"),
     )
 
-    print('='*20)
+    logging.info('='*50)
 
 
 if __name__ == "__main__":
